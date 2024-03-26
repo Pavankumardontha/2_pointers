6c454f4034a3ddf804ecb8314da89fd0a370733b
Pavan Kumar <pavankumardontha@gmail.com>
2024-03-27 04:07:47 +0530

    Update Watering_plants_2.cpp

--- Watering_plants_2.cpp
+++ Watering_plants_2.cpp
@@ -1,71 +1,69 @@
 class Solution {
 public:
-    int minimumRefill(vector<int>& a, int capacityA, int capacityB) 
+    int minimumRefill(vector<int>& p, int capacityA, int capacityB) 
     {
-        int n = a.size();
-        if(n==1 or n==2)
-        return 0;
-        int start = 0;
-        int end = n-1;
+        int n = p.size();
+        int l = 0;
+        int r = n-1;
         int ans = 0;
         int current_capacity_a = capacityA;
         int current_capacity_b = capacityB;
-        while(start<=end)
+        /*
+        At each step we water at both l and r. 
+        if l != r then l will be watered by alice and r will be watered by bob
+        if l ==r then alice will water if he has enough if not bob will water
+        */
+        while(l<=r)
         {
-            if(start==end)
+            if(l==r)
             {
-                if(current_capacity_a>=current_capacity_b)
+                if(current_capacity_a >= current_capacity_b)
                 {
-                    //alice should water the plant
-                    if(current_capacity_a < a[start])
-                    {
-                        ans++;
-                        start++;
-                    }
+                    // alice should water the plant
+                    if(current_capacity_a >= p[l])
+                    current_capacity_a = current_capacity_a - p[l];
                     else
-                        start++;
+                    {
+                        current_capacity_a = capacityA - p[l];
+                        ans++;
+                    }
                 }
                 else
                 {
-                    if(current_capacity_b < a[start])
-                    {
-                        ans++;
-                        end--;
-                    }
+                    // bob should water the plant
+                    if(current_capacity_b >= p[l])
+                    current_capacity_b = current_capacity_b - p[l];
                     else
-                        end--;
+                    {
+                        current_capacity_b = capacityB - p[l];
+                        ans++;
+                    }
                 }
-
             }
             else
             {
-                if(current_capacity_a>=a[start])
-                {
-                    current_capacity_a = current_capacity_a - a[start];
-                    start++;
-                }
+                /* l will be watered by alice and r will be watered by bob */
+                // water l by alice
+                if(current_capacity_a >= p[l])
+                current_capacity_a = current_capacity_a - p[l];
                 else
                 {
-                    current_capacity_a = capacityA;
-                    current_capacity_a = current_capacity_a - a[start];
-                    start++;
-                    ans++;
-                }
-                if(current_capacity_b>=a[end])
-                {
-                    current_capacity_b = current_capacity_b - a[end];
-                    end--;
-                }
-                else
-                {
-                    current_capacity_b = capacityB;
-                    current_capacity_b = current_capacity_b - a[end];
-                    end--;
-                    ans++;
+                    current_capacity_a = capacityA - p[l];
+                    ans++; // increment if we are refilling
                 }
 
+                // water r by bob
+                if(current_capacity_b >= p[r])
+                current_capacity_b = current_capacity_b - p[r];
+                else
+                {
+                    current_capacity_b = capacityB - p[r];
+                    ans++;
+                }
             }
+            l++;
+            r--;
         }
-        return ans;  
+        return ans;
     }
 };
