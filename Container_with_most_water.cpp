4c0b6af9ddaebffa2f51cc23146b07b905ce5196
Pavan Kumar <pavankumardontha@gmail.com>
2024-03-27 02:35:04 +0530

    Update Container_with_most_water.cpp

--- Container_with_most_water.cpp
+++ Container_with_most_water.cpp
@@ -1,20 +1,48 @@
+/*
+    l=0
+    r=n-1
+    We start l from left and r from right. We find the minimum of (a[l],a[r]). if a[l] is
+    less than a[r] , then the possible area is a[l]*(r-l+1). 
+    Now we have to decide what to do in below 3 cases 
+    1) a[l] < a[r]
+    2) a[l] > a[r] 
+    3) a[l] == a[r]
+    ex : [6,8,5,9,7] 
+          0 1 2 3 4
+    initially l=0 , r=4 and a[0]<a[4]
+    area = min(a[l],a[r])*(r-l) = min(6,7) * 4 = 24
+    In this case , we increment i , in other words we eliminate the pairs 
+    (6,9) , area = 6*3 = 18
+    (6,5) , area = 5*2 = 10 
+    (6,8) , area = 6*1 = 6
+    So the max area is formed between (6,7) pair. 
+    So if 
+    1) a[l] < a[r] , we can surely tell that this is the max possible area formed by 
+        using a[l] and so we need to increment l. 
+    2) a[r] < a[l] , we can surely tell that this is the max possible area formed by 
+        using a[r] and so we decrement r.
+    3) a[l] == a[r] , we need to increment l and decrement r.
+*/
+
 class Solution {
 public:
     int maxArea(vector<int>& a) 
     {
-        /* here 2 pointer technique is applied even if the array is not sorted !!! */
-        int n = a.size();
-        int start = 0;
-        int end = n-1;
-        int ans = INT_MIN;
-        while(start<end)
+        int l=0;
+        int r=a.size()-1;
+        int ans = 0; 
+        while(l<=r)
         {
-            ans = max(ans,(end-start)*min(a[start],a[end]));
-            /* if a[start] is less then increment start and if a[end] is less then decrement end.*/
-            if(a[start]>a[end])
-            end--;
+            ans = max(ans,min(a[l],a[r])*(r-l));
+            if(a[l]<a[r])
+            l++;
+            else if(a[r]<a[l])
+            r--;
             else
-            start++;
+            {
+                l++;
+                r--;
+            }
         }
         return ans;
     }
