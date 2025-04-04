eba66d0b44547f04f634cdb8fd2e6061ceb5449b
Pavan Kumar <pavankumardontha@gmail.com>
2025-04-05 03:50:44 +0530

    Update 3_sum.cpp

--- 3_sum.cpp
+++ 3_sum.cpp
@@ -85,59 +85,61 @@ public:
 /* Approach - 3 two pointers very very important */
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& a)
+    vector<vector<int>> threeSum(vector<int>& nums) 
     {
         /* 
-        1.for applying 2 pointers , our array should be sorted.This is convinient case. Sometimes the array might not also be sorted and we can apply 2 pointers
-        2. two pointers will only let you to find pair (a,b) consisting of 2 elements satisfying our constraints
-        3. out of (a,b,c) we need to atleast fix one element and apply 2 pointers to search for other 2  
+            1.for applying 2 pointers , our array should be sorted.This is convinient case. Sometimes the array might not 
+            also  be sorted and we can apply 2 pointers
+            2. two pointers will only let you to find pair (a,b) consisting of 2 elements satisfying our constraints
+            3. out of (a,b,c) we need to atleast fix one element and apply 2 pointers to search for other 2  
         */
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        int n = a.size();
-        sort(a.begin(),a.end());
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-2;i++)
         {
-            if(i!=0 and a[i]==a[i-1])
-            {
-                continue; // since we do not want duplicates
-            }
-            /*
-            if we fix c = a[i] , we will search for (a,b) in range (i,n-1) such that 
-            b + c = -a[i] and for this we apply 2 pointers
+            /* if we fix c = a[i] , we will search for (a,b) in range (i,n-1) such that  a+b+c=0; 
+            There are 2 places in which we need to handle the duplicate cases. 
+            a) at the outer loop 
+            b) inside the while loop of the 2 pointers
             */
+            // handle duplicate cases
+            if(i!=0 and nums[i] == nums[i-1])
+            continue; 
+
             int start = i+1;
             int end = n-1;
-            while(start < end)
+            while(start<end)
             {
-                int k = a[start] + a[end];
-                if(a[i] + k == 0) // c + (a + b) = 0 
+                int cur_sum = nums[i] + nums[start] + nums[end];
+                if(cur_sum == 0)
                 {
-                    vector<int> choices;
-                    choices.push_back(a[i]);
-                    choices.push_back(a[start]);
-                    choices.push_back(a[end]);
-                    ans.push_back(choices);
-                    /*
-                    we need to increment start and decrement end such that same pair(a,b) will not occur again if this is not taken care of , 
-                    then we will get duplicates.
-                    */
-                    while(start+1< n and a[start]==a[start+1])
+                    vector<int> temp = {nums[i],nums[start],nums[end]};
+                    ans.push_back(temp);
+                    // handling duplicate elements 
+                    while(start+1<n and nums[start]==nums[start+1])
                     start++;
-                    while(end-1>=0 and a[end] == a[end-1])
+                    while(end-1>=0 and nums[end] == nums[end-1])
                     end--;
+                    
                     start++;
                     end--;
                 }
-                else if(a[i] + k < 0)
+                else if(cur_sum < 0)
                 {
-                    while(start+1< n and a[start]==a[start+1])
+                    // we need to increment start to increase the cur_sum 
+                    // handling duplicates
+                    while(start+1<n and nums[start+1]==nums[start])
                     start++;
+                    
                     start++;
                 }
                 else
                 {
-                    while(end-1>=0 and a[end] == a[end-1])
+                    // handling duplicates
+                    while(end-1>=0 and nums[end] == nums[end-1])
                     end--;
+                    
                     end--;
                 }
             }
@@ -145,9 +147,3 @@ public:
         return ans;
     }
 };
-
-
-
-
-
-
