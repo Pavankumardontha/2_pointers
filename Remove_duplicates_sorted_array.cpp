b8e887a29394b43e750ceb071685f9c40d4c3afb
Pavan Kumar <pavankumardontha@gmail.com>
2025-04-09 19:53:39 +0530

    Update Remove_duplicates_sorted_array.cpp

--- Remove_duplicates_sorted_array.cpp
+++ Remove_duplicates_sorted_array.cpp
@@ -1,35 +1,28 @@
+ /*
+consider the example elements= 1,1,2,2,2,2,3,3
+                        index= 0 1 2 3 4 5 6 7
+notice the change in the from index 1->2 and from index 5->6. So we have encountered 2 changes and the solution revolves around finding these changes !! 
+*/
+
 class Solution {
 public:
-    int removeDuplicates(vector<int>& a) 
+    int removeDuplicates(vector<int>& nums) 
     {
-        int n =a.size();
-        int next_to_take = 0;
-        /*
-        consider the example 
-                elements= 1,1,2,2,2,2,3,3
-                   index= 0 1 2 3 4 5 6 7
+        int n = nums.size();
+        int start = 0;
+        int end = 1;
 
-        notice the change in the from index 1->2 and from index 5->6. So we have encountered 2 changes
-        and the solution revolves around finding these changes !! 
-        */
-        int index_to_change_next = 0;
-        while(next_to_take<n)
+        while(start<n and end<n)
         {
-            if(next_to_take ==0 or a[next_to_take-1]!=a[next_to_take])
+            if(nums[end]==nums[start])
+            end++;
+            else
             {
-                /* 
-                condition - 1 : We need to always take the first element. 
-                condition - 2 : if condition- 2 is satisfied , then element at next_to_take index is distinct. We need to store next_to_take 
-                index element and store it at index_to_change_next index.
-                */
-                a[index_to_change_next]=a[next_to_take];
-                index_to_change_next++;
-                next_to_take++;
+                start++;
+                nums[start] = nums[end];
+                end++;
             }
-            else 
-            next_to_take++;
         }
-        int no_of_distinct_elements = index_to_change_next;
-        return no_of_distinct_elements;
+        return start+1;
     }
 };
