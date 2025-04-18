a9403b6d9ad9b352c4033d25d7a174fc3800291d
Pavan Kumar <pavankumardontha@gmail.com>
2025-04-18 11:52:35 +0530

    Create lexographically_smallest_palindrome.cpp

---
+++ lexographically_smallest_palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) 
+    {
+        int start = 0;
+        int end = s.length()-1;
+        while(start<end)
+        {
+            if(s[start] != s[end])
+            {
+                if(s[start]<s[end])
+                s[end] = s[start];
+                else
+                s[start]=s[end];
+            }
+            start++;
+            end--;
+        }
+        return s;
+    }
+};
